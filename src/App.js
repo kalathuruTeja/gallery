f4ea8e89c64c9d5463a9157b1e0bb04cbd14e48a
kalathurutejaa <kalathuruteja7883@gmail.com>
2022-11-17 13:25:38 +0530

    mauin

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="heading">Gallery snapchats</h1>
+      <center>
+        <input type="search"  placeholder="Enter search name"/> <br/> <br/>
+        <input type= "submit" name="search" />
+      </center>
+      
+    </div>
+  );
+}
+
+export default App;
